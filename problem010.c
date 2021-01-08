10218816dd0318f268ec7daecdd7b7925723305a
Taha Yasin Çakmak <tycakmak1@gmail.com>
2021-01-08 17:37:32 +0300

    0104

--- problem010.c
+++ problem010.c
@@ -1,3 +1,7 @@
+/*          ###QUESTION###
+The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+
+Find the sum of all the primes below two million.*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
